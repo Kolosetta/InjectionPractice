70c9abada1d7252c0aee5034aa83db08aaf1a276
tr00per <kolosovskiy91@gmail.com>
2022-08-28 06:48:42 +0300

    ViewModelFactory добавлен ApplicationScope, чтобы она была лишь в одном экземпляре В фабрике теперь используется провайдер ВьюМоделей, чтобы при каждом вызове create получать новый объект конкретног окласса Вьюмодели

--- app/src/main/java/com/example/injectionpractice/example2/presentation/ViewModelFactory.kt
+++ app/src/main/java/com/example/injectionpractice/example2/presentation/ViewModelFactory.kt
@@ -2,13 +2,16 @@ package com.example.injectionpractice.example2.presentation
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import com.example.injectionpractice.example2.data.di.ApplicationScope
 import javax.inject.Inject
+import javax.inject.Provider
 
+@ApplicationScope
 class ViewModelFactory @Inject constructor(
-    private val viewModels: @JvmSuppressWildcards Map<String, ViewModel>
+    private val viewModels: @JvmSuppressWildcards Map<String, Provider<ViewModel>>
 ) : ViewModelProvider.Factory {
 
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
-        return viewModels[modelClass.simpleName] as T
+        return viewModels[modelClass.simpleName]?.get() as T
     }
 }
